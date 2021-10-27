214ba20003280c2c561849ed06c9f89642584fa9
Xiang Tan <xtan@uccs.edu>
2021-10-27 13:44:18 -0600

    Added coin collection with inaccessible blocks

--- homework6/coincollectinaccessible.py
+++ homework6/coincollectinaccessible.py
@@ -5,6 +5,7 @@
 # This implementation uses the idea in homework 6.
 # Find maximum number of coins, which can be collected by robots.
 # Robot can only move to the right or down.
+# -1 means the ceil is inaccessible for the robot.
 
 f = [
     [0, 0, 0, 0, 0, 0],
@@ -14,14 +15,15 @@ f = [
     [0, 0, 0, 0, 0, 0],
 ]
 # result of test array supposed to get
-check = [
-    [0, 0, 0, 0, 1, 1],
-    [0, 1, 1, 2, 2, 2],
-    [0, 1, 1, 3, 3, 4],
-    [0, 1, 2, 3, 3, 5],
-    [1, 1, 2, 3, 4, 5],
-]
 
+# check if the selected ceil should be inaccessible.
+# return -1 if it is
+# otherwise return the ceil value + the add value
+def CheckInacc(check, add) :
+    if check == -1 :
+        return -1
+    else :
+        return check + add
 
 def CoinCollection(Array) :
     # measure/store size of rows and columns
@@ -30,12 +32,13 @@ def CoinCollection(Array) :
 
     # initialize the value of [0, 0] point.
     f[0][0] = Array[0][0]
+
     # fill first row
     for i in range(colsize) :
-        f[0][i] = f[0][i - 1] + Array[0][i]
+        f[0][i] = CheckInacc(f[0][i - 1], Array[0][i])
     
     for i in range(1, rowsize) :
-        f[i][1] = f[i - 1][i] + Array[i][1]
+        f[i][1] = CheckInacc(f[i - 1][i], Array[i][1])
         #print(f[i])
         for j in range(1, colsize) :
             #print(f[i - 1][j], f[i][j - 1])
@@ -46,11 +49,11 @@ def CoinCollection(Array) :
 def main() :
     ## Test Arrays
     c = [
-        [0, 0, 0, 0, 1, 0],
-        [0, 1, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0, 1],
-        [0, 0, 1, 0, 0, 1],
-        [1, 0, 0, 0, 1, 0],
+        [0, -1, 0, 1, 0, 0],
+        [1, 0, 0, -1, 1, 0],
+        [0, 1, 0, -1, 1, 1],
+        [0, 0, 1, 1, 0, 1],
+        [-1, -1, -1, 0, 1, 0],
     ]
     collected = CoinCollection(c)
     print("Total coin collected: ", collected)
